c6cb7b53343f128f86a63b49b76a420391504bca
rodrigo <rodriguinho-1993@hotmail.com>
2018-04-04 21:13:04 -0300

    Mostrar dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ContratoService } from './services/contrato.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ContratoComponent } from './contrato/contrato.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { AlunoService } from './services/aluno.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContratoComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ContratoService, AlunoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/domain/aluno.ts
@@ -0,0 +1,9 @@
+export interface Aluno{
+    id_descente : String;
+    nome : String;
+    ano_ingresso : String;
+    periodo_ingresso : String;
+    nivel : String;
+    id_forma_ingresso : String;
+    id_curso : String;
+}
